214cde815edb69ab9b518306568df85b9f88379a
Bhavya Singh <bhavyasingh2611@outlook.com>
2026-03-30 12:08:17 +0530

    remove profiler, encoding is not the bottle neck the gpu is

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -3,6 +3,4 @@ from .control_api import start_control_server as start_control_server
 from .losses import DualLoss as DualLoss
 from .losses import PolicyLoss as PolicyLoss
 from .losses import ValueLoss as ValueLoss
-from .profiler import StepProfiler as StepProfiler
-from .profiler import benchmark as benchmark
 from .trainer import Trainer as Trainer
